2fd60ffc761e87d28a19881572e009475db3c56c
ganesh <ganeshsadhashivam@outlook.com>
2022-12-11 22:31:05 +0530

    reviews

---
+++ src/data.js
@@ -0,0 +1,44 @@
+const reviews = [
+  {
+    id: 1,
+    name: "ganesh",
+    job: "web developer",
+    image:
+      "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDJ8fHBlcnNvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1000&q=60",
+    text: "I am very Much Excited To Learn New Technologies From FreeCodeCamp and from other Resources using Internet",
+  },
+  {
+    id: 2,
+    name: "April",
+    job: "Fashion Designer",
+    image:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZmFjZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1000&q=60",
+    text: "I am a Fashion Designer ",
+  },
+  {
+    id: 3,
+    name: "Alina",
+    job: "artist",
+    image:
+      "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHNtaWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+    text: "I am Artist ",
+  },
+  {
+    id: 4,
+    name: "Kristi",
+    job: "Finance",
+    image:
+      "https://images.unsplash.com/photo-1580465446361-8aae5321522b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHNtaWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+    text: "I am Finance Advisor",
+  },
+  {
+    id: 5,
+    name: "Jake",
+    job: "Youtuber",
+    image:
+      "https://images.unsplash.com/photo-1548213238-0da7521bd6e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fHNtaWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+    text: "I am Traveller",
+  },
+];
+
+export default reviews;
